db6e2b0b5e523f778a0453df88d7ddbeefcf0773
CreamySushi <152081964+CreamySushi@users.noreply.github.com>
2026-05-23 00:28:45 +0800

    Delete db_config.py

--- db_config.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-def db_connection():
-    return MySQLdb.connect (
-        host = "localhost",
-        user = "root",
-        passwd = "#Uphsl123",
-        database = "hotel_db",
-    )
