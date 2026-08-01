1326c68c781dc4453c255098774790a552c96c23
CreamySushi <johnaaronflavien@gmail.com>
2026-08-01 19:46:27 +0800

    add resume path to view my resume

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div style={{ width: '100%', height: '899px' }}>
+            <object data='../src/assets/Quirante-Resume.pdf' width="100%" height="100%" />
+        </div>
+    )
+}
+export default Resume
