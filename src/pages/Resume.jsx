59f03f5f8a87523263e55a134a5c8126c72380ab
CreamySushi <johnaaronflavien@gmail.com>
2026-08-01 20:53:36 +0800

    i put the file to public to be able to read by vercel

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
     return (
         <div style={{ width: '100%', height: '899px' }}>
-            <object data='../src/assets/Quirante-Resume.pdf' width="100%" height="100%" />
+            <object data='../public/Quirante-Resume.pdf' width="100%" height="100%" />
         </div>
     )
 }
